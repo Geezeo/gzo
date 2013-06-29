390ba42aafed0cb7d956d0c7300013e2d68b2b64
Fuzz Leonard <fuzz@fuzzleonard.com>
2013-06-28 18:26:32 -0700

    Add second test 'it gets the sum of allaccount balances'

--- spec/geezeo_spec.rb
+++ spec/geezeo_spec.rb
@@ -2,13 +2,18 @@ require "spec_helper"
 
 describe "Geezeo accounts" do
   before(:each) do
-    @client = Geezeo.client(credentials["api_key"], credentials["user_id"])
+    @geezeo = Geezeo.client(credentials["api_key"], credentials["user_id"])
   end
 
-
   it "gets a list of accounts" do
-    @account = @client.accounts.all.first
+    @account = @geezeo.accounts.all.first
 
     @account.id.should_not be_nil
+  end
+
+  it "gets the sum of all account balances" do
+    @balance = @geezeo.accounts.sum_of_balances
+
+    @balance.should be_kind_of(Float)
   end
 end
