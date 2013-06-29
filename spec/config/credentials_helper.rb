d337fc4f5ea19ee56a1f3b100b2c9feb248526aa
Fuzz Leonard <fuzz@fuzzleonard.com>
2013-06-29 02:59:29 -0700

    Use consistent style

--- spec/config/credentials_helper.rb
+++ spec/config/credentials_helper.rb
@@ -1,15 +1,15 @@
 module CredentialsHelper
   def self.credentials
     credentials_file = "#{File.dirname(__FILE__)}/geezeo_credentials.json"
-    unless File.exist? credentials_file
+    unless File.exist?(credentials_file)
       raise "You need credentials in config/geezeo_credentials.json"
     end
-    @credentials ||= json_load credentials_file
+    @credentials ||= json_load(credentials_file)
   end
   def credentials; CredentialsHelper.credentials; end
 
   def self.json_load(file)
-    json = File.read file
-    JSON.load json
+    json = File.read(file)
+    JSON.load(json)
   end
 end
