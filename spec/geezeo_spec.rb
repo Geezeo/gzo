2457ef02fac05edff6f66ecc471aa380f820daca
Fuzz Leonard <fuzz@fuzzleonard.com>
2013-07-09 01:34:28 -0700

    Set up guard

--- spec/geezeo_spec.rb
+++ spec/geezeo_spec.rb
@@ -10,14 +10,16 @@ describe "Geezeo accounts", :vcr do
   it "gets a list of all accounts" do
     account = @geezeo.accounts.all.first
 
-    account.id.should_not be_nil
+    account.id.should be_kind_of(Integer)
+    account.name.should be_kind_of(String)
   end
 
   it "gets a single account" do
     account_id = @geezeo.accounts.all.first.id
     account = @geezeo.accounts.find(account_id)
 
-    account.id.should_not be_nil
+    account.id.should be_kind_of(Integer)
+    account.name.should be_kind_of(String)
   end
 
   it "gets the sum of all account balances" do
@@ -30,14 +32,16 @@ describe "Geezeo accounts", :vcr do
     account = @geezeo.accounts.all.first
     transaction = account.transactions.first
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
+    transaction.nickname.should be_kind_of(String)
   end
 
   it "returns a list of its recent transactions" do
     account = @geezeo.accounts.all.first
     transaction = account.recent_transactions.first
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
+    transaction.nickname.should be_kind_of(String)
   end
 
   # TODO: make this test less brittle
@@ -47,7 +51,7 @@ describe "Geezeo accounts", :vcr do
     transaction = account.transactions(last_transaction_id).first
     last_transaction = account.transactions(last_transaction_id).last
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
     last_transaction.id.should eq("4471f06a-a3ac-4d7c-95c3-402203623c25")
   end
 end
@@ -62,20 +66,23 @@ describe "Geezeo transactions", :vcr do
   it "gets a list of all transactions" do
     transaction = @geezeo.transactions.all.first
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
+    transaction.nickname.should be_kind_of(String)
   end
 
   it "gets a list of all recent transactions" do
     transaction = @geezeo.transactions.all(:recent).first
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
+    transaction.nickname.should be_kind_of(String)
   end
 
   it "gets a single page of transactions" do
     account = @geezeo.accounts.all.first
     transaction = @geezeo.transactions.find(account).first
 
-    transaction.id.should_not be_nil
+    transaction.id.should be_kind_of(String)
+    transaction.nickname.should be_kind_of(String)
   end
 end
 
